0eb7b9a69a27b3658a7461370ec895c9182a13ce
VladVelik <sosinvladik@yandex.ru>
2023-05-16 13:57:37 +0300

    Initial Commit

---
+++ LeisurioApp/LeisurioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeisurioAppApp.swift
+//  LeisurioApp
+//
+//  Created by Sosin Vladislav on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LeisurioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
